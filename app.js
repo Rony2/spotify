d207abceb3aa1b3f5cee93685ef6a8992096bf0b
Rony2 <ronakpatelg5@gmail.com>
2019-02-23 03:00:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,9 +26,9 @@ app.use('/searchSongs', searchSongs);
 
 // error handler
 app.use(function (err, req, res, next) {
-  //res.header('Access-Control-Allow-Origin', '*');
-  //res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  //res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
 
   // set locals, only providing error in development
   res.locals.message = err.message;
