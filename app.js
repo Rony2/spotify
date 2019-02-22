de27dbb0f4c483871f0efee0225f6b60c42c1769
Rony2 <ronakpatelg5@gmail.com>
2019-02-23 03:12:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use('/searchSongs', searchSongs);
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
     next();
 })
