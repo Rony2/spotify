7b56f70bec1ce595e75e2b0b18da71e9f70660aa
kmmaruthi06@gmail.com <kmmaruthi06@gmail.com>
2019-02-23 02:55:44 +0530

    added cros origin

--- app.js
+++ app.js
@@ -26,6 +26,10 @@ app.use('/searchSongs', searchSongs);
 
 // error handler
 app.use(function (err, req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
